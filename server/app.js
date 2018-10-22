ed08ece5b82eb870cd88edfcee7ac4e8ea570e00
jezbread <jezreelcallejas@jezbreads-MacBook-Pro.local>
2018-10-22 12:15:25 -0700

    Basic express server setup

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const bestPlaces = require('./data');
 
+data = (req, res) => {
+    res.send(bestPlaces);
+}
+
+success = (req, res) => {
+    res.send('Success!');
+}
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use('/', express.static(__dirname), morgan('dev'));
+app.get('/data', data);
+app.get('/', success);
 
 // finally export the express application
 module.exports = app;
